49f426c2dec05b51905ddd3d09687c9a5e8e88fc
XLRSeatingBuck <nautiyal1987amish@gmail.com>
2025-01-28 17:42:32 -0800

    make dpad work

--- Assets/WheelDevice.cs
+++ Assets/WheelDevice.cs
@@ -51,7 +51,11 @@ public struct LogitechG920InputReport : IInputStateTypeInfo
     /// <summary>
     /// default = 0b1000
     /// </summary>
-    [InputControl(layout = "Dpad", bit = 0, sizeInBits = 4)]
+    [InputControl(name = "dpad", format = "BIT", layout = "Dpad", sizeInBits = 4, defaultState = 8)]
+    [InputControl(name = "dpad/up", format = "BIT", layout = "DiscreteButton", parameters = "minValue=7,maxValue=1,nullValue=8,wrapAtValue=7", bit = 0, sizeInBits = 4)]
+    [InputControl(name = "dpad/right", format = "BIT", layout = "DiscreteButton", parameters = "minValue=1,maxValue=3", bit = 0, sizeInBits = 4)]
+    [InputControl(name = "dpad/down", format = "BIT", layout = "DiscreteButton", parameters = "minValue=3,maxValue=5", bit = 0, sizeInBits = 4)]
+    [InputControl(name = "dpad/left", format = "BIT", layout = "DiscreteButton", parameters = "minValue=5, maxValue=7", bit = 0, sizeInBits = 4)]
     [FieldOffset(1)] public byte dpad;
 
     [InputControl(layout = "Button", bit = 7)]
