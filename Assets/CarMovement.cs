a77ce6b31aef517c2e1ea49f7b09af460ce8e778
JohnCorby <williamforgames6@gmail.com>
2025-02-11 00:15:26 -0800

    better car debug

--- Assets/CarMovement.cs
+++ Assets/CarMovement.cs
@@ -5,6 +5,8 @@ public class CarMovement : MonoBehaviour
 {
 	public InputActionReference GasInput, BrakeInput, SteerInput;
 
+	public Rigidbody Body;
+
 	public WheelCollider LeftFront, RightFront, LeftBack, RightBack;
 
 	public float GasTorque, BrakeTorque, SteerAngle;
@@ -15,8 +17,6 @@ public class CarMovement : MonoBehaviour
 		var brake = BrakeInput.action.ReadValue<float>();
 		var steer = SteerInput.action.ReadValue<float>();
 
-		Debug.Log($"gas = {gas}, brake = {brake}, steer = {steer}");
-
 		LeftBack.motorTorque = gas * GasTorque;
 		LeftBack.brakeTorque = brake * BrakeTorque;
 		RightBack.motorTorque = gas * GasTorque;
@@ -26,5 +26,18 @@ public class CarMovement : MonoBehaviour
 
 		RightFront.steerAngle = steer * SteerAngle;
 		LeftFront.steerAngle = steer * SteerAngle;
+	}
+
+	private void OnGUI()
+	{
+		var gas = GasInput.action.ReadValue<float>();
+		var brake = BrakeInput.action.ReadValue<float>();
+		var steer = SteerInput.action.ReadValue<float>();
+		GUILayout.Label($"gas = {gas}\nbrake = {brake}\nsteer = {steer}");
+
+		GUILayout.Label($"wheel rpm = \n{LeftFront.rpm}\t{RightFront.rpm}\n{LeftBack.rpm}\t{RightBack.rpm}");
+
+		GUILayout.Label($"velocity = {Body.linearVelocity} / {transform.InverseTransformDirection(Body.linearVelocity)}\n" +
+			$"angular velocity = {Body.angularVelocity} / {transform.InverseTransformDirection(Body.angularVelocity)}");
 	}
 }
