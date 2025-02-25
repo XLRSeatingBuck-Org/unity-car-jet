13388536fdb571ef7ace88f70eb4b3db8cbda4dc
XLRSeatingBuck <nautiyal1987amish@gmail.com>
2025-02-24 18:31:12 -0800

    motor on impact

--- Assets/HapticsTest.cs
+++ Assets/HapticsTest.cs
@@ -1,12 +1,25 @@
 using Bhaptics.SDK2;
+using System.Linq;
 using UnityEngine;
 
 public class HapticsTest : MonoBehaviour
 {
 	private void Update()
 	{
-		var x = (int)(Mathf.Sin(Time.time) * 50 + 50);
-		Debug.Log(x);
-		BhapticsLibrary.PlayMotors((int)PositionType.GloveR, new[] { x, x, x, x, x, x }, 100);
+		var motorPower = (int)(Mathf.Sin(Time.time) * 50 + 50);
+		motorPower /= 5;
+		Debug.Log(motorPower);
+		BhapticsLibrary.PlayMotors((int)PositionType.GloveL, Enumerable.Repeat(motorPower, 6).ToArray(), 100);
+		BhapticsLibrary.PlayMotors((int)PositionType.GloveR, Enumerable.Repeat(motorPower, 6).ToArray(), 100);
+		BhapticsLibrary.PlayMotors((int)PositionType.Vest, Enumerable.Repeat(motorPower, 32).ToArray(), 100);
+	}
+
+	public void OnImpact(Vector3 impulse)
+	{
+		var motorPower = 100;
+		Debug.Log(motorPower);
+		BhapticsLibrary.PlayMotors((int)PositionType.GloveL, Enumerable.Repeat(motorPower, 6).ToArray(), 1000);
+		BhapticsLibrary.PlayMotors((int)PositionType.GloveR, Enumerable.Repeat(motorPower, 6).ToArray(), 1000);
+		BhapticsLibrary.PlayMotors((int)PositionType.Vest, Enumerable.Repeat(motorPower, 32).ToArray(), 1000);
 	}
 }
