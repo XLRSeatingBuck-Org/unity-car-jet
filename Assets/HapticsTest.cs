dd00f66f6d2bdba30f44d7f72652cbd099a56953
XLRSeatingBuck <nautiyal1987amish@gmail.com>
2025-02-24 18:46:07 -0800

    update haptic test again

--- Assets/HapticsTest.cs
+++ Assets/HapticsTest.cs
@@ -14,12 +14,15 @@ public class HapticsTest : MonoBehaviour
 		BhapticsLibrary.PlayMotors((int)PositionType.Vest, Enumerable.Repeat(motorPower, 32).ToArray(), 100);
 	}
 
-	public void OnImpact(Vector3 impulse)
+	// triggered on RIGIDBODY, not collider
+	private void OnCollisionEnter(Collision other)
 	{
+		Debug.Log($"impact {other.impulse}");
 		var motorPower = 100;
-		Debug.Log(motorPower);
-		BhapticsLibrary.PlayMotors((int)PositionType.GloveL, Enumerable.Repeat(motorPower, 6).ToArray(), 1000);
-		BhapticsLibrary.PlayMotors((int)PositionType.GloveR, Enumerable.Repeat(motorPower, 6).ToArray(), 1000);
-		BhapticsLibrary.PlayMotors((int)PositionType.Vest, Enumerable.Repeat(motorPower, 32).ToArray(), 1000);
+		// Debug.Log(motorPower);
+		// more motor power overrides less, no matter the time
+		BhapticsLibrary.PlayMotors((int)PositionType.GloveL, Enumerable.Repeat(motorPower, 6).ToArray(), 100);
+		BhapticsLibrary.PlayMotors((int)PositionType.GloveR, Enumerable.Repeat(motorPower, 6).ToArray(), 100);
+		BhapticsLibrary.PlayMotors((int)PositionType.Vest, Enumerable.Repeat(motorPower, 32).ToArray(), 100);
 	}
 }
