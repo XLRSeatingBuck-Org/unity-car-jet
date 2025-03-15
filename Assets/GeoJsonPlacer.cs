d65dc9af85afd8a506527be49c10a202c5c3ddc2
Will Corby <williamforgames6@gmail.com>
2025-03-14 18:55:01 -0700

    start geo json loader

---
+++ Assets/GeoJsonPlacer.cs
@@ -0,0 +1,23 @@
+﻿using CesiumForUnity;
+using System;
+using UnityEngine;
+
+public class GeoJsonLoader : MonoBehaviour
+{
+	public GameObject PrefabToPlace;
+
+	private void Awake()
+	{
+		// load the dataset
+		{
+
+		}
+
+		// place prefab
+		{
+			var cesiumGeoreference = GetComponentInParent<CesiumGeoreference>();
+
+
+		}
+	}
+}
