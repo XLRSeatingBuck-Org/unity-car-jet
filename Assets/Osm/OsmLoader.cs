6af4300407bdccf19bd52923abcb7177fd0e7bf2
JohnCorby <williamforgames6@gmail.com>
2025-03-29 23:50:03 -0700

    filter out buildings and forests

--- Assets/Osm/OsmLoader.cs
+++ Assets/Osm/OsmLoader.cs
@@ -22,10 +22,39 @@ public class OsmLoader : MonoBehaviour
 			// https://github.com/SimonCuddihy/osm-unity/blob/master/Assets/Scripts/MapReader.cs
 			var xml = new XmlDocument();
 			xml.LoadXml(XmlToLoad.text);
-			foreach (XmlNode node in xml.SelectNodes("/osm/node"))
-			{
-				longLatHeightPoints.Add(new double3(double.Parse(node.Attributes["lon"].Value), double.Parse(node.Attributes["lat"].Value), 0));
-			}
+			var nodes = xml.SelectNodes("osm/node").Cast<XmlNode>()
+				.ToDictionary(
+					node => ulong.Parse(node.Attributes["id"].Value),
+					node => new double3(double.Parse(node.Attributes["lon"].Value), double.Parse(node.Attributes["lat"].Value), 0)
+				);
+			// Debug.Log(string.Join("\n", nodes));
+			
+			var buildings = xml.SelectNodes("osm/way[tag[@k='building']]").Cast<XmlNode>()
+				.Select(way =>
+				{
+					var count = 0;
+					var total = double3.zero;
+					foreach (var node in way.SelectNodes("nd").Cast<XmlNode>())
+					{
+						var id = ulong.Parse(node.Attributes["ref"].Value);
+						var nodePos = nodes[id];
+						total += nodePos;
+						count++;
+					}
+
+					return total / count;
+				})
+				.ToList();
+			// Debug.Log(string.Join("\n", buildings));
+
+			var forests = xml.SelectNodes("osm/way[tag[@k='natural']]").Cast<XmlNode>()
+				.Select(way => way.SelectNodes("nd").Cast<XmlNode>()
+					.Select(node => nodes[ulong.Parse(node.Attributes["ref"].Value)])
+					.ToList()
+				)
+				.ToList();
+
+			longLatHeightPoints = buildings;
 		}
 		Debug.Log($"dataset load took {stopwatch.ElapsedMilliseconds}ms");
 
