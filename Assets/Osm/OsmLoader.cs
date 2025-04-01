91233c5da42b2177d18370088860c463a7ce8253
JohnCorby <williamforgames6@gmail.com>
2025-03-31 21:54:21 -0700

    forest generation with grid of points

--- Assets/Osm/OsmLoader.cs
+++ Assets/Osm/OsmLoader.cs
@@ -17,7 +17,8 @@ public class OsmLoader : MonoBehaviour
 	{
 		// load the dataset
 		var stopwatch = Stopwatch.StartNew();
-		var longLatHeightPoints = new List<double3>();
+		var buildings = new List<double3>();
+		var trees = new List<double3>();
 		{
 			// https://github.com/SimonCuddihy/osm-unity/blob/master/Assets/Scripts/MapReader.cs
 			var xml = new XmlDocument();
@@ -28,36 +29,53 @@ public class OsmLoader : MonoBehaviour
 					node => new double3(double.Parse(node.Attributes["lon"].Value), double.Parse(node.Attributes["lat"].Value), 0)
 				);
 			// Debug.Log(string.Join("\n", nodes));
-			
-			var buildings = xml.SelectNodes("osm/way[tag[@k='building']]").Cast<XmlNode>()
-				.Select(way =>
-				{
-					var count = 0;
-					var total = double3.zero;
-					foreach (var node in way.SelectNodes("nd").Cast<XmlNode>().SkipLast(1))
-					{
-						var id = ulong.Parse(node.Attributes["ref"].Value);
-						var nodePos = nodes[id];
-						total += nodePos;
-						count++;
-					}
 
-					return total / count;
-				})
-				.ToList();
+			foreach (var way in xml.SelectNodes("osm/way[tag[@k='building']]").Cast<XmlNode>())
+			{
+				var count = 0;
+				var total = double3.zero;
+				foreach (var node in way.SelectNodes("nd").Cast<XmlNode>().SkipLast(1))
+				{
+					var id = ulong.Parse(node.Attributes["ref"].Value);
+					var nodePos = nodes[id];
+					total += nodePos;
+					count++;
+				}
+
+				buildings.Add(total / count);
+			}
 			// Debug.Log(string.Join("\n", buildings));
 
-			var forests = xml.SelectNodes("osm/way[tag[@k='natural']]").Cast<XmlNode>()
-				.Select(way => way.SelectNodes("nd").Cast<XmlNode>().SkipLast(1)
+			foreach (var way in xml.SelectNodes("osm/way[tag[@k='natural']]").Cast<XmlNode>())
+			{
+				var boundary = way.SelectNodes("nd").Cast<XmlNode>().SkipLast(1)
 					.Select(node => nodes[ulong.Parse(node.Attributes["ref"].Value)])
-					.ToList()
-				)
-				.ToList();
-			// TODO: choose random points in the polygon
+					.ToList();
+				var (minlat, minlon, maxlat, maxlon) = (
+					boundary.Min(x => x.y),
+					boundary.Min(x => x.x),
+					boundary.Max(x => x.y),
+					boundary.Max(x => x.x)
+				);
+				trees.AddRange(boundary);
 
-			longLatHeightPoints = buildings;
+				var points = new List<double3>();
+				const double step = 0.001;
+				for (var i = minlon; i <= maxlon; i += step)
+				{
+					for (var j = minlat; j <= maxlat; j += step)
+					{
+						var point = new double3(i, j, 0);
+						if (PolygonHelper.IsPointInPolygon(boundary, point))
+							points.Add(point);
+					}
+				}
+
+				trees.AddRange(points);
+			}
+
 		}
-		Debug.Log($"dataset load took {stopwatch.ElapsedMilliseconds}ms");
+		Debug.Log($"dataset load took {stopwatch.ElapsedMilliseconds}ms with {trees.Count} trees and {buildings.Count} buildings");
 
 		// place prefab
 		stopwatch.Restart();
@@ -66,10 +84,10 @@ public class OsmLoader : MonoBehaviour
 			var tileset = georeference.GetComponentInChildren<Cesium3DTileset>();
 			
 			// https://github.com/CesiumGS/cesium-unity/pull/507#issuecomment-2380048726
-			var task = tileset.SampleHeightMostDetailed(longLatHeightPoints.ToArray());
+			var task = tileset.SampleHeightMostDetailed(trees.Concat(buildings).ToArray());
 			yield return new WaitForTask(task);
 			var result = task.Result;
-			Debug.Log($"sample success = {result.sampleSuccess.All(x => x)}");
+			Debug.Log($"sample success = {result.sampleSuccess.All(x => x)} in {stopwatch.ElapsedMilliseconds}ms");
 			foreach (var longLatHeightPoint in result.longitudeLatitudeHeightPositions)
 			{
 				var prefab = Instantiate(PrefabToPlace, georeference.transform);
---
+++ Assets/Osm/PolygonHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Unity.Mathematics;
+
+// https://stackoverflow.com/questions/71021941/how-to-generate-a-random-vector3-point-inside-a-polygon
+// https://stackoverflow.com/questions/4243042/c-sharp-point-in-polygon
+public static class PolygonHelper
+{
+    public static bool IsPointInPolygon(List<double3> polygon, double3 testPoint)
+    {
+        bool result = false;
+        int j = polygon.Count - 1;
+        for (int i = 0; i < polygon.Count; i++)
+        {
+            if (polygon[i].y < testPoint.y && polygon[j].y >= testPoint.y || 
+                polygon[j].y < testPoint.y && polygon[i].y >= testPoint.y)
+            {
+                if (polygon[i].x + (testPoint.y - polygon[i].y) /
+                    (polygon[j].y - polygon[i].y) *
+                    (polygon[j].x - polygon[i].x) < testPoint.x)
+                {
+                    result = !result;
+                }
+            }
+            j = i;
+        }
+        return result;
+    }
+
+}
