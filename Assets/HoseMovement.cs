488a1f38277de171e0e3100b15993016a1403bf0
XLRSeatingBuck <nautiyal1987amish@gmail.com>
2025-04-09 18:53:54 -0700

    hose input

--- Assets/HoseMovement.cs
+++ Assets/HoseMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class HoseMovement : MonoBehaviour
 {
@@ -7,34 +8,39 @@ public class HoseMovement : MonoBehaviour
 
     // Reference to the water GameObject
     public GameObject waterObject;
+    
+    public InputActionReference HoseAimInput, HoseFireInput;
 
     // Update is called once per frame
     void Update()
     {
+        var hoseAim = HoseAimInput.action.ReadValue<Vector2>();
+        var hoseFire = HoseFireInput.action.IsPressed();
+        
         // Get the current rotation of the gameObject (only the Z-axis will be affected)
         Vector3 currentRotation = transform.rotation.eulerAngles;
 
         // Check if the K key is being held down to rotate left (decrease Z-axis rotation)
-        if (Input.GetKey(KeyCode.K))
+        if (hoseAim.x < 0)
         {
             // Rotate left around the Z-axis
             transform.rotation = Quaternion.Euler(currentRotation.x, currentRotation.y, currentRotation.z - rotationSpeed * Time.deltaTime);
         }
         // Check if the L key is being held down to rotate right (increase Z-axis rotation)
-        else if (Input.GetKey(KeyCode.L))
+        else if (hoseAim.x > 0)
         {
             // Rotate right around the Z-axis
             transform.rotation = Quaternion.Euler(currentRotation.x, currentRotation.y, currentRotation.z + rotationSpeed * Time.deltaTime);
         }
 
         // When the F key is pressed, activate the water GameObject
-        if (Input.GetKeyDown(KeyCode.F))
+        if (hoseFire)
         {
             waterObject.SetActive(true);
         }
 
         // When the F key is released, deactivate the water GameObject
-        if (Input.GetKeyUp(KeyCode.F))
+        if (!hoseFire)
         {
             waterObject.SetActive(false);
         }
