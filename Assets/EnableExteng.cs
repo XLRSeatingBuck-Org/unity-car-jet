6dce7e8f6229161daa794dd1631329cbd6c83a54
XLRSeatingBuck <nautiyal1987amish@gmail.com>
2025-05-24 19:27:02 -0700

    Full WildFire System

--- Assets/EnableExteng.cs
+++ Assets/EnableExteng.cs
@@ -1,46 +1,85 @@
-using System;
+// using System;
+// using UnityEngine;
+// using UnityEngine.InputSystem;
+//
+// public class EnableExteng : MonoBehaviour
+// {
+//     // Array to hold references to 8 GameObjects
+//     public GameObject[] particleObjects = new GameObject[8];
+//
+//     public InputActionReference UseInput;
+//
+//     private void Awake()
+//     {
+//         foreach (var particleObject in particleObjects)
+//         {
+//             particleObject.SetActive(false);
+//         }
+//     }
+//
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         // Enable all objects when 'P' is pressed down
+//         if (UseInput.action.WasPressedThisFrame())
+//         {
+//             foreach (GameObject obj in particleObjects)
+//             {
+//                 if (obj != null)
+//                 {
+//                     obj.SetActive(true);
+//                 }
+//             }
+//         }
+//
+//         // Disable all objects when 'P' is released
+//         if (UseInput.action.WasReleasedThisFrame())
+//         {
+//             foreach (GameObject obj in particleObjects)
+//             {
+//                 if (obj != null)
+//                 {
+//                     obj.SetActive(false);
+//                 }
+//             }
+//         }
+//     }
+// }
+
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class EnableExteng : MonoBehaviour
 {
-    // Array to hold references to 8 GameObjects
     public GameObject[] particleObjects = new GameObject[8];
-
-    public InputActionReference UseInput;
 
     private void Awake()
     {
         foreach (var particleObject in particleObjects)
         {
-            particleObject.SetActive(false);
+            if (particleObject != null)
+                particleObject.SetActive(false);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // Enable all objects when 'P' is pressed down
-        if (UseInput.action.WasPressedThisFrame())
+        if (Input.GetKeyDown(KeyCode.P))
         {
             foreach (GameObject obj in particleObjects)
             {
                 if (obj != null)
-                {
                     obj.SetActive(true);
-                }
             }
         }
 
         // Disable all objects when 'P' is released
-        if (UseInput.action.WasReleasedThisFrame())
+        if (Input.GetKeyUp(KeyCode.P))
         {
             foreach (GameObject obj in particleObjects)
             {
                 if (obj != null)
-                {
                     obj.SetActive(false);
-                }
             }
         }
     }
---
+++ Assets/GasCollision.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class GasCollision : MonoBehaviour
+{
+    void OnParticleCollision(GameObject other)
+    {
+        if (other.CompareTag("Fire"))
+        {
+            Debug.Log("Collision detected with fire!");
+
+            // Scale the fire GameObject to (0, 0, 0)
+            other.transform.localScale = Vector3.zero;
+
+            // Loop through all children of Fire
+            foreach (Transform child in other.transform)
+            {
+                // Skip scaling if the child is SmokeEffect
+                if (child.name == "SmokeEffect")
+                {
+                    // Get the ParticleSystem and start adjusting emission
+                    ParticleSystem smoke = child.GetComponent<ParticleSystem>();
+                    if (smoke != null)
+                    {
+                        StartCoroutine(AdjustSmokeEmission(smoke));
+                    }
+                    continue;
+                }
+
+                // Scale all other children to (0, 0, 0)
+                child.localScale = Vector3.zero;
+            }
+        }
+    }
+
+    // Coroutine to gradually reduce the emission rate
+    private IEnumerator AdjustSmokeEmission(ParticleSystem smoke)
+    {
+        var emission = smoke.emission;
+        float currentRate = 100f;
+        emission.rateOverTime = currentRate;
+
+        while (currentRate > 0)
+        {
+            yield return new WaitForSeconds(0.5f);
+            currentRate -= 10f;
+            emission.rateOverTime = Mathf.Max(0, currentRate);
+        }
+    }
+}
