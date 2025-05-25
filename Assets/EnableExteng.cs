7e416312580697c1fe0d0c651c5da4c18aae00dd
JohnCorby <williamforgames6@gmail.com>
2025-05-24 23:43:39 -0700

    use input system

--- Assets/EnableExteng.cs
+++ Assets/EnableExteng.cs
@@ -1,85 +1,46 @@
-// using System;
-// using UnityEngine;
-// using UnityEngine.InputSystem;
-//
-// public class EnableExteng : MonoBehaviour
-// {
-//     // Array to hold references to 8 GameObjects
-//     public GameObject[] particleObjects = new GameObject[8];
-//
-//     public InputActionReference UseInput;
-//
-//     private void Awake()
-//     {
-//         foreach (var particleObject in particleObjects)
-//         {
-//             particleObject.SetActive(false);
-//         }
-//     }
-//
-//     // Update is called once per frame
-//     void Update()
-//     {
-//         // Enable all objects when 'P' is pressed down
-//         if (UseInput.action.WasPressedThisFrame())
-//         {
-//             foreach (GameObject obj in particleObjects)
-//             {
-//                 if (obj != null)
-//                 {
-//                     obj.SetActive(true);
-//                 }
-//             }
-//         }
-//
-//         // Disable all objects when 'P' is released
-//         if (UseInput.action.WasReleasedThisFrame())
-//         {
-//             foreach (GameObject obj in particleObjects)
-//             {
-//                 if (obj != null)
-//                 {
-//                     obj.SetActive(false);
-//                 }
-//             }
-//         }
-//     }
-// }
-
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EnableExteng : MonoBehaviour
 {
+    // Array to hold references to 8 GameObjects
     public GameObject[] particleObjects = new GameObject[8];
+
+    public InputActionReference UseInput;
 
     private void Awake()
     {
         foreach (var particleObject in particleObjects)
         {
-            if (particleObject != null)
-                particleObject.SetActive(false);
+            particleObject.SetActive(false);
         }
     }
 
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
         // Enable all objects when 'P' is pressed down
-        if (Input.GetKeyDown(KeyCode.P))
+        if (UseInput.action.WasPressedThisFrame())
         {
             foreach (GameObject obj in particleObjects)
             {
                 if (obj != null)
+                {
                     obj.SetActive(true);
+                }
             }
         }
 
         // Disable all objects when 'P' is released
-        if (Input.GetKeyUp(KeyCode.P))
+        if (UseInput.action.WasReleasedThisFrame())
         {
             foreach (GameObject obj in particleObjects)
             {
                 if (obj != null)
+                {
                     obj.SetActive(false);
+                }
             }
         }
     }
