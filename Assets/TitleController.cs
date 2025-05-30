af2ca88f9478a8f6d4814fdb29c82d92775a68ee
JohnCorby <williamforgames6@gmail.com>
2025-05-29 22:27:40 -0700

    turn on vsync in title

--- Assets/TitleController.cs
+++ Assets/TitleController.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class TitleController : MonoBehaviour
 {
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 1; // force vsync on for now
+    }
+
     public void CarButtonPressed() => SceneManager.LoadScene(1);
     public void PlaneButtonPressed() => SceneManager.LoadScene(2);
 }
